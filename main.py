699d75ca835d004dc44c62e207988fa8f3c28239
saranya90ramanathan-source <saranya90ramanathan@gmail.com>
2026-03-28 11:17:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,14 +12,21 @@ def health():
     return {"status": "ok"}
 
 class CustomerInput(BaseModel):
-    AGE_YR_CT: float
-    CUST_GNDR_CD: str
-    CUST_TYPE_CD: str
-    CTZSHP_CNTRY1_CD: str
-    COUNTRY_OF_INC: str
-    RES_CNTRY_CD: str
-    PEP_FL: str
-    ANNL_INCM_BASE_AM: float
+    AGE_YR_CT : int
+    CUST_GNDR_CD : str
+    CUST_TYPE_CD : str
+    CTZSHP_CNTRY1_CD : str
+    CTZSHP_CNTRY2_CD : str
+    COUNTRY_OF_INC : str
+    RES_CNTRY_CD : str
+    PEP_FL : str
+    FRGN_ASSETS_FL : str
+    ANNL_INCM_BASE_AM : float
+    NET_WRTH_BASE_AM : float
+    LQD_NET_WRTH_BASE_AM : float
+    OCPTN_NM : str
+    DPNDT_QT : int
+    WLTH_SRC_DSCR_TX : str
 
 @app.post("/predict")
 def predict_risk(data: CustomerInput):
